16a0e3954c687789f39912b2ffa1fda86ce45b95
Seth Mwebi <sethmwebi27@gmail.com>
2021-08-25 13:28:04 +0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Head from "next/head"
+
+const About = () => {
+  return (
+    <>
+      <Head>
+          <title>Ninja List | About</title>
+          <meta keywords="ninja"/>
+        </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ea sit et assumenda nisi fugit possimus numquam officia perspiciatis! Deserunt architecto quam sint nesciunt adipisci ex repellat. Illum, nam, ex?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ea sit et assumenda nisi fugit possimus numquam officia perspiciatis! Deserunt architecto quam sint nesciunt adipisci ex repellat. Illum, nam, ex?</p>
+      </div>
+    </>
+  )
+}
+
+export default About
